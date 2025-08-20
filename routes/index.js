4872e81e60946636a3ebeb96008da08fb31e00cb
Manthan-Pelne <manthanpelne1997@gmail.com>
2025-08-20 11:44:32 +0530

    added about-us page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import { getIndex } from "../controllers/index.js";
 const router = express.Router();
 
 router.get('/', getIndex);
+router.get("/about-us/",(req,res)=>{ res.render("about-us")})
 
 
 export default router;

fd312834abac8a87b3beb286d2bbe0d0b9220722
Manthan-Pelne <manthanpelne1997@gmail.com>
2025-08-29 17:55:36 +0530

    updated responsiveness

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', getIndex);
 router.get("/about-us/",(req,res)=>{ res.render("about-us")})
-
+router.get("/contact-us/",(req,res)=>{ res.render("contact-us")})
+router.get("/frequently-asked-questions/",(req,res)=>{ res.render("faq")})
 
 export default router;
